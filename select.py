5b6e900fa14fb1c4b2b3e3af1ef5bb67136c3447
siddharth bhavsar <siddharth2395@gmail.com>
2017-09-05 15:17:22 +0530

    removed extra imports

--- select.py
+++ select.py
@@ -1,6 +1,4 @@
 import pyodbc
-import random
-import string
 con = pyodbc.connect('Trusted_Connection=yes', driver = '{ODBC Driver 13 for SQL Server}',server = 'localhost\SQLEXPRESS', database = 'TestDB')
 cursor = con.cursor()
 cursor.execute("select * from person")
