e9e5c9d9442b732e4f034c3eac6e672411ba1806
siddharth bhavsar <siddharth2395@gmail.com>
2017-09-05 15:16:05 +0530

    Persons renamed to person

--- insert.py
+++ insert.py
@@ -3,10 +3,10 @@ import random
 import string
 con = pyodbc.connect('Trusted_Connection=yes', driver = '{ODBC Driver 13 for SQL Server}',server = 'localhost\SQLEXPRESS', database = 'TestDB')
 cursor = con.cursor()
-for i in range(1,10):
+for i in range(10):
 	lname="".join( [random.choice(string.letters) for i in xrange(5)])
 	fname="".join( [random.choice(string.letters) for i in xrange(5)])
 	city="".join( [random.choice(string.letters) for i in xrange(5)])
-	cursor.execute("insert into Persons(LastName, FirstName, City) values ('%s', '%s', '%s')" % (lname,fname,city))
+	cursor.execute("insert into person(lastname, firstname, city) values ('%s', '%s', '%s')" % (lname,fname,city))
 con.commit()
 con.close()
--- select.py
+++ select.py
@@ -3,7 +3,7 @@ import random
 import string
 con = pyodbc.connect('Trusted_Connection=yes', driver = '{ODBC Driver 13 for SQL Server}',server = 'localhost\SQLEXPRESS', database = 'TestDB')
 cursor = con.cursor()
-cursor.execute("select * from Persons")
+cursor.execute("select * from person")
 for row in cursor.fetchall():
 	print row
 con.close()
